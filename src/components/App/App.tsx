88622e343351d0e015514bb5d013620837dc2860
Topmewan <anon8733@gmail.com>
2023-02-05 18:55:27 +0500

    add new api for work on deploy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,8 +2,6 @@ import { Accordion, Form, Navbar, Todo } from "..";
 import { useGetNews, useNews, useTodos } from "../../hooks";
 import { UIBox, UICheckbox, UITypography } from "../../ui";
 
-import axios from "axios";
-import { useEffect } from "react";
 import Marquee from "react-fast-marquee";
 import { getRandomItemInArray } from "../../config/helpers";
 import styles from "./App.module.css";
@@ -17,30 +15,10 @@ export const App = () => {
 		onChangeData,
 		todayTodosVisible,
 		onChangeTodayTodosVisible,
-		addTodo,
 	} = useTodos();
 
 	const randomNew = data?.articles ? getRandomItemInArray(data.articles) : null;
 
-	useEffect(() => {
-		const options = {
-			method: "GET",
-			url: "https://api.newscatcherapi.com/v2/search",
-			params: { q: "Bitcoin", lang: "en", sort_by: "relevancy", page: "1" },
-			headers: {
-				"x-api-key": "Et4nwCwGUOsE64oWbc4Zs6G1CFBT95vYxLhf5L3nWEU",
-			},
-		};
-
-		axios
-			.request(options)
-			.then(function (response) {
-				console.log(response.data);
-			})
-			.catch(function (error) {
-				console.error(error);
-			});
-	}, []);
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.container}>
--- src/config/api/services.ts
+++ src/config/api/services.ts
@@ -1,33 +1,27 @@
-import axios from "axios";
-
-const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${
-	import.meta.env.VITE_API_KEY
-}`;
+import axios, { AxiosRequestConfig } from "axios";
 
 export type ReqModel = {
 	status?: string;
-	totalResults?: number;
 	articles?: Article[];
 };
 
 export type Article = {
-	source?: Source;
-	author?: string;
 	title?: string;
-	description?: string;
-	url?: string;
-	urlToImage?: string;
-	publishedAt?: Date;
-	content?: string;
 };
 
-export type Source = {
-	id?: string;
-	name?: string;
+const config: AxiosRequestConfig = {
+	params: { q: "Bitcoin", lang: "en", sort_by: "relevancy", page: "1" },
+	headers: {
+		"x-api-key": import.meta.env.VITE_API_KEY,
+	},
 };
+
 export const NEWS_SERVICE = {
 	async getNew() {
-		const { data } = await axios.get<ReqModel>(url);
+		const { data } = await axios.get<ReqModel>(
+			import.meta.env.VITE_API_URL,
+			config
+		);
 		return data;
 	},
 };
